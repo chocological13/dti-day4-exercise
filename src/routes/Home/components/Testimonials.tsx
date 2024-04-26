987f57779eb1d2674f76d12de37cd0400d1260e5
nina <annisamustofa@outlook.com>
2024-04-26 14:28:27 +0700

    feat: added testimonials

--- src/routes/Home/components/Testimonials.tsx
+++ src/routes/Home/components/Testimonials.tsx
@@ -1,7 +1,35 @@
 import "../style.css";
+import profile1 from "../../../assets/testimonial-1.png";
+import profile2 from "../../../assets/testimonial-2.png";
 
 const Testimonials: React.FC = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className="testi-container">
+      <h3 className="testitle">Testimonials</h3>
+      <div className="testimonials">
+        <div className="testi-item">
+          <p className="testi-text">"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni eos aliquid quo iste atque temporibus repellendus earum illum! Corporis minima quia reprehenderit dignissimos ea ut doloribus est, ducimus itaque distinctio!"</p>
+          <div className="testi-profile">
+            <img src={profile1} alt="ayush" />
+            <div className="testi-name">
+              <p className="profile-name">Ayush Raj</p>
+              <p className="profile-role">VP of Marketing @ Webflow</p>
+            </div>
+          </div>
+        </div>
+        <div className="testi-item">
+          <p className="testi-text">"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni eos aliquid quo iste atque temporibus repellendus earum illum! Corporis minima quia reprehenderit dignissimos ea ut doloribus est, ducimus itaque distinctio!"</p>
+          <div className="testi-profile">
+            <img src={profile2} alt="alex" />
+            <div className="testi-name">
+              <p className="profile-name">Alex Cattoni</p>
+              <p className="profile-role">Founder @ CopyPossy</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
